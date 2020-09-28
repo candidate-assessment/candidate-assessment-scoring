79a48b722ea5c0b3387252a0b2ad7a9dd03089ea
Ryan Fisch <rfisch05@gmail.com>
2020-09-28 17:17:21 -0400

    candidate X score 20200927

---
+++ bloomfilter.1/app.js
@@ -0,0 +1,31 @@
+import { BloomFilter} from "bloomfilter";
+
+var fs = require('fs');
+var records = JSON.parse(fs.readFileSync('array.json', 'utf8'));
+
+let bloom = new BloomFilter(7188794, 10);
+
+
+console.time('aaaa')
+records.forEach(element => {
+    const data = element.EmailAddress + element.FirstNameLastName;
+    if (bloom.test(data))
+    {
+        console.log('DUP', element)
+
+    
+    } else {
+       bloom.add(data)
+    }
+});
+console.timeEnd('aaaa')
+
+
+
+
+// bloom.add('abc');
+//  console.log(bloom.test('abcd'));
+//  console.log(bloom.test('abc'));
+
+
+
---
+++ bloomfilter.1/dist/app.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var _bloomfilter = require('bloomfilter');
+
+var fs = require('fs');
+var records = JSON.parse(fs.readFileSync('array.json', 'utf8'));
+
+var bloom = new _bloomfilter.BloomFilter(7188794, 10);
+
+console.time('aaaa');
+records.forEach(function (element) {
+    var data = element.EmailAddress + element.FirstNameLastName;
+    if (bloom.test(data)) {
+        console.log('DUP', element);
+    } else {
+        bloom.add(data);
+    }
+});
+console.timeEnd('aaaa');
+
+// bloom.add('abc');
+//  console.log(bloom.test('abcd'));
+//  console.log(bloom.test('abc'));
